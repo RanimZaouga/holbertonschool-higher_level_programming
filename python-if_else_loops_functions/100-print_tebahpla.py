f347eb4735fc53241632637917f5a6f4e4abd686
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-09-26 18:09:32 +0000

    hell loops

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in reversed(range(97, 123)):
-    if (i % 2 == 0):
-        print('{:c}'.format(i), end='')
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        print(chr(i), end='')
     else:
-        print('{:c}'.format(i - 32), end='')
+        print(chr(i - 32), end='')
+
+print()
