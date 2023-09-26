a804f82d6ff9e4b97b1e6993ed3c2b2e8a32e488
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-09-26 15:16:21 +0000

    hell loops

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,7 @@ if(number < 0):
     ld = int(str(number)[-1]) * -1
 else:
     ld = int(str(number)[-1])
+    
 if ld > 5:
     print(f"Last digit of {number} is {ld} and is greater than 5")
 elif ld < 6 and ld != 0:
