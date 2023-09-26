b388049bad5d16f6abd65f74e844de97515e73b6
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-09-26 17:28:02 +0000

    hell loops

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}".format(i), end=", " if i < 99 else "\n")
-
+for num in range(0, 100):
+        if num != 99:
+                print("{:02d}, ".format(num), end='')
+        else:
+                print("{:02d}".format(num))
