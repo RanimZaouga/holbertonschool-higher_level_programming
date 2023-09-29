73241023c92571353f17ac52d8ad44d3743531e6
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-09-29 20:22:48 +0000

    import & modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-def infinite_add(a, b, c):
-    while True:
-        a = a + b + c
+def infinite_add(*args):
+    result = sum(int(arg) for arg in args)
+    print(result)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 3-infinite_add.py <a> <b> <c>")
-        sys.exit(1)
+    args = sys.argv[1:]
+    infinite_add(*args)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[2])
-    c = int(sys.argv[3]))
-
-    infinite_add(a, b, c)
-    
