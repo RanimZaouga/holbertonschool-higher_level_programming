6317c5d63cf5f845deece81f49868f033a3b8158
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-09-29 20:08:05 +0000

    import & modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
-    e = sys.argv
-    sum = 0
-    for i in range(1, len(e)):
-        sum += int(e[i])
-    print("{}".format(sum))
+def infinite_add(*args):
+    result = sum(int(arg) for arg in args)
+    print(result)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    infinite_add(*args)
+
