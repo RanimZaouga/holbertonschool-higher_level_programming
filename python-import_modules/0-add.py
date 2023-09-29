f9c29405816b9abb76b71299d4499d5fbc0e920d
RANIM <135613007+RanimZaouga@users.noreply.github.com>
2023-09-29 20:18:25 +0000

    import & modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+# Import the add function from the file add_0.py
+from add_0 import add
+
+# Assign the values 1 and 2 to the variables a and b
+a = 1
+b = 2
+
+# Call the add function with the arguments a and b
+result = add(a, b)
+
+# Print the result of the addition
+print(f"{a} + {b} = {result}")
+
